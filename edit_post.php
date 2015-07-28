d3e79af980f78b2f8e3c91211347b98148d348c4
telswick <telswick@austin.rr.com>
2015-07-27 23:07:38 -0500

    Attempting to add functionality to add posts to posts table in blog database via mysql

--- edit_post.php
+++ edit_post.php
@@ -31,6 +31,8 @@
 
 include 'db_info.php';
 
+
+// in the database named blog with tables called posts and tags
 // include database connection stuff in db_info.php
 // could use require_once instead (but remember parentheses)
 // can't include the same file twice if you use require_once
@@ -45,10 +47,12 @@ include 'db_info.php';
 <form action="edit_post.php"
       method="POST">
     Enter your blog post....<br>
-    <input type="text" name="title" placeholder="Title your post"><br>
-    <input type="text" name="author" placeholder="Who are you?"><br>
-    <input type="datetime" name="date"><br>
-    <textarea name="contents" rows="25" cols="80" onclick="this.innerhtml=''">Write something!</textarea><br><br>
+    <input style="color:coral" type="text" name="title" placeholder="Title your post"><br>
+    <input style="color:coral" type="text" name="author" placeholder="Who are you?"><br>
+
+    <textarea style="background-color:lightblue"  name="contents"
+              rows="25" cols="80" onclick="this.innerhtml=' '">
+        Write something!</textarea><br><br>
     <input type="submit" name="submit" value="Submit Blog Post"><br>
 
 </form>
@@ -60,8 +64,42 @@ include 'db_info.php';
 if (isset($_POST['submit']))  {
     $title = $_POST['title'];
     $author = $_POST['author'];
+    $mydate = date("d F Y H:i:s");
     $contents = $_POST['contents'];
+// }
+
+// copying from hw8, todo_list
+
+$stmt = $db->prepare("INSERT INTO posts (title, author, date, contents) VALUES (?, ?, ?, ?)");
+echo $db->error;
+
+// if (!isset($_SESSION['list'])) {
+//     $_SESSION['list'] = array();
+// }
+
+// if (isset($_POST['submit'])) {
+    // add the text in 'todo'
+    // to the SESSION variable 'list'
+    // array_push($_SESSION['list'],
+    //            $_POST['todo']);
+
+    //$stmt = $db->prepare("INSERT INTO todo_list ('item', 'assigndate') VALUES (?, ?)");
+    //$stmt = $db->prepare("INSERT INTO todo_list (item) VALUES (?)");
+
+
+    // $mydate =  date("d F Y H:i:s");
+    // $todo = $_POST['todo'];
+
+
+    // $stmt->bind_param("s", $todo);
+    $stmt->bind_param("ssss", $title, $author, $mydate, $contents);
+    // $stmt->close();
+
+//actually run statement with the parameters we've substituted, how to add a row
+
+    $stmt->execute();
 }
+
 
 
 
